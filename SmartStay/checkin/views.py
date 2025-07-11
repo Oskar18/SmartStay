d2266bd50136cd03def2d58752fd49dacce5ea9a
Oskar18 <775334585@seznam.cz>
2025-07-11 17:50:50 +0200

    guest_from_hotovson

--- SmartStay/checkin/views.py
+++ SmartStay/checkin/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .forms import GuestForm
 
 def home(request):
     return render(request, 'checkin/home.html')
+
+def guest_checkin(request):
+    if request.method == 'POST':
+        form = GuestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'checkin/success.html')
+    else:
+        form = GuestForm()
+
+    return render(request, 'checkin/guest_form.html', {'form': form})
